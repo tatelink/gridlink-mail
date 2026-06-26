e349f3af6945843fdb7b3418b249769cfb0dff5c
emon <emon@masto.top>
2026-06-27 00:23:56 +0200

    Release 1.0.7 (versionCode 108)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "app.sterna"
         minSdk = 26
         targetSdk = 36
-        versionCode = 107
-        versionName = "1.0.6"
+        versionCode = 108
+        versionName = "1.0.7"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
