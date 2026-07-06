fa0d7c578b44a7eb0f36a399289537b5e1afc306
emon <emon@masto.top>
2026-07-07 00:32:23 +0200

    Release 1.1.2 (129)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "app.sterna"
         minSdk = 26
         targetSdk = 36
-        versionCode = 128
-        versionName = "1.1.1"
+        versionCode = 129
+        versionName = "1.1.2"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
